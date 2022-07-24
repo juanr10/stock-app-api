bd9cb69a07602ed5e289547bf0911d560f6e5c66
juanr10 <argudojuan7@gmail.com>
2022-07-24 18:41:31 +0200

    add get product stock test

---
+++ tests/Feature/Api/ProductsStockTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductsStockTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_get_the_stock_from_a_product()
+    {
+        $product = Product::factory()->create([
+            "name" => "Anillo Desire",
+            "description" => "Quo dicta hic eum ad unde ad et velit.",
+            "price" => 20.54,
+        ]);
+
+        $this->json('GET', 'api/products/' . $product->id . '/stock', ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'data' =>  $product->stock->quantity
+            ]);
+    }
+}
