e366707cf86dd9934fc5b420d02751cfa85bed66
juanr10 <argudojuan7@gmail.com>
2022-07-24 18:47:28 +0200

    add can get all products with the stock test

--- tests/Feature/Api/ProductsStockTest.php
+++ tests/Feature/Api/ProductsStockTest.php
@@ -32,4 +32,51 @@ class ProductsStockTest extends TestCase
         $this->json('GET', 'api/products/1/stock', ['Accept' => 'application/json'])
             ->assertStatus(404);
     }
+
+    /** @test */
+    public function can_get_all_products_with_the_stock()
+    {
+        $product1 = Product::factory()->create([
+            "name" => "Anillo Desire",
+            "description" => "Quo dicta hic eum ad unde ad et velit.",
+            "price" => 20.54,
+        ]);
+
+        $product2 = Product::factory()->create([
+            "name" => "Pulsera Icy",
+            "description" => "Quo dicta hic eum ad unde ad et velit.",
+            "price" => 54.18
+        ]);
+
+        $this->json('GET', 'api/products/stock', ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    [
+                        "name" => $product1->name,
+                        "description" => $product1->description,
+                        "price" => $product1->price,
+                        "created_at" => $product1->created_at,
+                        "updated_at" => $product1->updated_at,
+                        "stock" => [
+                            'product_id' => $product1->stock->product_id,
+                            'quantity' =>  $product1->stock->quantity,
+                            'updated_at' => $product1->stock->updated_at,
+                        ]
+                    ],
+                    [
+                        "name" => $product2->name,
+                        "description" => $product2->description,
+                        "price" => $product2->price,
+                        "created_at" => $product2->created_at,
+                        "updated_at" => $product2->updated_at,
+                        "stock" => [
+                            'product_id' => $product2->stock->product_id,
+                            'quantity' =>  $product2->stock->quantity,
+                            'updated_at' => $product2->stock->updated_at,
+                        ]
+                    ],
+                ]
+            ]);
+    }
 }
