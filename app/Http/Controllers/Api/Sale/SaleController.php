69456db88ce12df2dbc8fd255061027369b0da3e
juanr10 <argudojuan7@gmail.com>
2022-07-25 00:24:45 +0200

    add sale controller

---
+++ app/Http/Controllers/Api/Sale/SaleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Sale;
+
+use App\Models\Sale;
+use Illuminate\Http\Request;
+use App\Http\Requests\SaleRequest;
+use App\Http\Controllers\Controller;
+
+class SaleController extends Controller
+{
+    //
+}
