89eb12118a9ec397766de80dd01ecac22fd1ab29
juanr10 <argudojuan7@gmail.com>
2022-07-24 18:20:25 +0200

    add product model & migration

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price'];
+
+    protected $casts = [
+        'price' => 'float',
+    ];
+
+    public function getCreatedAtAttribute()
+    {
+        return date('d-m-Y H:i:s', strtotime($this->attributes['created_at']));
+    }
+
+    public function getUpdatedAtAttribute()
+    {
+        return date('d-m-Y H:i:s', strtotime($this->attributes['updated_at']));
+    }
+}
