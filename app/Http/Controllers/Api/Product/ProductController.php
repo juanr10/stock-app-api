ee9b085bd5eafb687385d33d209e89e37f4b7b83
juanr10 <argudojuan7@gmail.com>
2022-07-24 18:27:18 +0200

    add product controller

---
+++ app/Http/Controllers/Api/Product/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\Product;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    //
+}
