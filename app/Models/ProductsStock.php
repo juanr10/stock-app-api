1b7ac7e9fe6f99a512c0db0b5ccfed0db411cc97
juanr10 <argudojuan7@gmail.com>
2022-07-24 18:21:57 +0200

    add productsStock model & migration

---
+++ app/Models/ProductsStock.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductsStock extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['product_id', 'quantity'];
+
+    protected $hidden = ['id', 'created_at'];
+
+    public function getUpdatedAtAttribute()
+    {
+        return date('d-m-Y H:i:s', strtotime($this->attributes['updated_at']));
+    }
+}
