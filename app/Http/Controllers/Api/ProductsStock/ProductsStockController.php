c9836c9caa3d982c14ac7f15306d859639cca4b8
juanr10 <argudojuan7@gmail.com>
2022-07-24 18:28:12 +0200

    add productsStock controller

---
+++ app/Http/Controllers/Api/ProductsStock/ProductsStockController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\ProductsStock;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductsStockController extends Controller
+{
+    //
+}
