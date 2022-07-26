a95b52ab4d74b7a8ee8f044f4a9b504166133532
juan.argudo <juan.argudo@avannubo.com>
2022-07-26 17:13:22 +0200

    update store method to update product stock

--- app/Http/Controllers/Api/Sale/SaleController.php
+++ app/Http/Controllers/Api/Sale/SaleController.php
@@ -3,16 +3,39 @@
 namespace App\Http\Controllers\Api\Sale;
 
 use App\Models\Sale;
-use Illuminate\Http\Request;
 use App\Http\Requests\SaleRequest;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-
+use App\Application\ProductsStock\UpdateStock;
 class SaleController extends Controller
 {
     public function store(SaleRequest $request)
     {
-        return response()->json([
-            'data' => Sale::create($request->all())
-        ], 201);
+        $statusCode = 500;
+        $response = null;
+
+        DB::beginTransaction();
+
+        try {
+            $sale = Sale::create($request->all());
+            $productStock = new UpdateStock($sale->product->stock);
+            $productStock->decrease($sale->quantity);
+
+            DB::commit();
+
+            $statusCode = 201;
+
+            $response = [
+                'data' => $sale
+            ];
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            $response = [
+                'error' => $e
+            ];
+        }
+
+        return response()->json($response, $statusCode);
     }
 }
